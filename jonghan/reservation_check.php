07f2bbf72dd336e191cd52f59292f8d42825c7c7
KimJongHan <jong7721@naver.com>
2021-07-23 09:53:33 +0900

    예약발권

--- jonghan/reservation_check.php
+++ jonghan/reservation_check.php
@@ -1,11 +1,18 @@
 <?php
-    error_reporting(E_ALL);
-    ini_set('display_errors', FALSE);
+    // error_reporting(E_ALL);
+    // ini_set('display_errors', FALSE);
 
+    //로컬에서 사용하는 변수
+    // $host = "localhost";
+    // $user = "root";
+    // $pw = "1234";
+    // $dbName = "movie_kiosk";
+    
+    // 서버에서 사용하는 변수
     $host = "localhost";
-    $user = "root";
-    $pw = "1234";
-    $dbName = "movie_kiosk"; // 서버에서 사용할때는 "w1004mesmg"로 해야함 
+    $user = "w1004mesmg";
+    $pw = "sunmoons1s2s3!";
+    $dbName = "w1004mesmg"; // 서버에서 사용할때는 "w1004mesmg"로 해야함 
     $conn= mysqli_connect($host, $user, $pw, $dbName); //db 연결부분
 
     ?>
@@ -20,10 +27,10 @@
     $reservation_no = $_POST['reservation_no'];
     $sql = "SELECT reservation_no FROM ticket";
     $result = mysqli_query($conn, $sql);
-    echo "----존재하는 예매번호 표시----";
+    echo "----존재하는 예매번호 표시----<br>";
     $arry = array();
     while($row = mysqli_fetch_array($result)){
-        echo $row['reservation_no']."<br>";
+        echo $row['reservation_no']." ";
         $arry[] = $row['reservation_no'];
     }
 
@@ -36,14 +43,14 @@
     echo "입력한 예매 번호 확인: ";
     echo $reservation_no; 
     echo "<hr>";
-    if(array_search($reservation_no, $arry) === false ||array_search($reservation_no, $arry) === 0){
+    if(array_search($reservation_no, $arry) === false) {
         echo "예매번호 없음";
     }else{
         echo "예매확인 <br>";
         $sql = "SELECT * FROM ticket WHERE reservation_no=$reservation_no";
         $result = mysqli_query($conn, $sql);
         $row = mysqli_fetch_array($result);
-        echo $row['reservation_no']." ".$row['theater']." ".$row['price']." ".$row['title']." ".$row['seat']." ".$row['people']."<br>";
+        echo $row['reservation_no']." ".$row['theater']." ".$row['price']." ".$row['title']." ".$row['seat_1']." ".$row['seat_2']." ".$row['seat_3']." ".$row['seat_4']." ".$row['people']."<br>";
     }
 
     mysqli_close($conn);
