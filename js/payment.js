7083ad775c459733b11330600c5c11a0eb87ce21
siwmua0 <siwmua0@naver.com>
2021-07-30 11:02:53 +0900

    모달

--- js/payment.js
+++ js/payment.js
@@ -149,10 +149,7 @@ $(document).ready(function(){
   
   window.setTimeout(function(){$(".is-open").removeClass("is-open")}, 1200);
   
-  $(".btn").click(function(){
-    check = true;
-    $(".remove").click();
-  });
+ 
 
     
   
